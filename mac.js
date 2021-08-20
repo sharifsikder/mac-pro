6fe0ac9094d50d56c7ee0697fd0b851d97d083e0
sharifsikder <sharif01742088325@gmail.com>
2021-08-21 01:02:31 +0600

    style

--- mac.js
+++ mac.js
@@ -1,1 +1,36 @@
-console.log('Sharif Sikder js file ta kaj kortese')
+
+const largeMemory = document.getElementById('large-memory');
+const lowMemory = document.getElementById('low-memory');
+const memoryCost = document.getElementById('memory-cost');
+const lowStorage = document.getElementById('low-storage');
+const middleStorage =document.getElementById('middle-storage');
+const largeStorage = document.getElementById('large-storage');
+const storageCost = document.getElementById('storage-cost');
+
+// button memory
+largeMemory.addEventListener('click', function(){
+
+    memoryCost.innerText = '180';
+})
+lowMemory.addEventListener('click', function(){
+
+    memoryCost.innerText = '0';
+})
+// button storage
+lowStorage.addEventListener('click', function(){
+    
+    storageCost.innerText = '0';
+})
+
+middleStorage.addEventListener('click', function(){
+    
+    storageCost.innerText = '100';
+
+})
+
+largeStorage.addEventListener('click', function(){
+    
+    storageCost.innerText = '180';
+})
+
+
